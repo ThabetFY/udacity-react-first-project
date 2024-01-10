b6e6f7cb046e51546ff390a2d21b7199ec8ecd14
Thabet Alyahya <ThabetFy@outlook.com>
2024-01-10 09:32:03 +0300

    feat: made search pgae works
    Adding functonalty to Serach page to make it work by using useEffect and seacrh from  booksAPI

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,7 +1,48 @@
 import { Link } from "react-router-dom";
 import Book from "../components/Book";
+import { useEffect, useState } from "react";
+import * as BooksAPI from "../utils/BooksAPI";
 
 const SearchPage = ({ books }) => {
+  const [query, setQuery] = useState("");
+  const [searchedBooks, setSearchedBooks] = useState([]);
+
+  const updateQuery = (query) => {
+    setQuery(query.trim());
+  };
+
+  useEffect(() => {
+    let unmounted = false;
+
+    const searchBooks = async () => {
+      try {
+        const results = await BooksAPI.search(query);
+
+        const updatedResults = results.map((resultBook) => {
+          const match = books.find((book) => book.id === resultBook.id);
+          if (!match) {
+            return { ...resultBook, shelf: "none" };
+          }
+          return resultBook;
+        });
+
+        setSearchedBooks(updatedResults);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (!unmounted) {
+      searchBooks();
+    }
+
+    return () => {
+      unmounted = true;
+    };
+  }, [query]);
+
+  const displayedBooks = searchedBooks || books;
+
   return (
     <div className="search-books">
       <div className="search-books-bar">
@@ -9,17 +50,22 @@ const SearchPage = ({ books }) => {
           Close
         </Link>
         <div className="search-books-input-wrapper">
-          <input type="text" placeholder="Search by title, author, or ISBN" />
+          <input
+            type="text"
+            placeholder="Search by title, author, or ISBN"
+            value={query}
+            onChange={(event) => updateQuery(event.target.value)}
+          />
         </div>
       </div>
       <div className="search-books-results">
         <ol className="books-grid">
-          {books.map((book) => (
+          {displayedBooks.map((book) => (
             <Book
               key={book.id}
               title={book.title}
               authors={book.authors}
-              bookCover={book.imageLinks.thumbnail}
+              bookCover={book.imageLinks?.thumbnail}
               shelf={book.shelf}
               changeShelf={() => {}}
             />
